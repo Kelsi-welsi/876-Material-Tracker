8cde227efd7a0784af6a4462c3a1748e1270489e
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-02-21 14:56:56 -0500

    Edited Add Product Template

--- AddProduct.java
+++ AddProduct.java
@@ -1,19 +1,23 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class AddProduct extends JFrame implements ActionListener{
     private JLabel nameLabel;
-    private JLabel descLabel;
     private JLabel priceLabel;
+    private JLabel descLabel;
     private JTextField nameTextField;
-    private JTextField descTextField;
     private JTextField priceTextField;
+    private JTextArea descTextArea;
     private JButton addButton;
 
     public AddProduct(){
         setTitle("Add Material");
+        setSize(750, 500);
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
         JPanel productPanel = new JPanel();
 
@@ -23,18 +27,22 @@ public class AddProduct extends JFrame implements ActionListener{
         productPanel.add(nameTextField);
 
         descLabel = new JLabel("Description: ");
-        descTextField = new JTextField();
+        descTextArea = new JTextArea();
         productPanel.add(descLabel);
-        productPanel.add(descTextField);
+        productPanel.add(descTextArea);
 
         priceLabel = new JLabel("Price: ");
         priceTextField = new JTextField();
         productPanel.add(priceLabel);
         productPanel.add(priceTextField);
-        
+        add(productPanel);
+
+        c.weighty  = 0.05f;
+        c.gridx = 0;
+        c.gridy = 4;
         addButton = new JButton("Save");
         addButton.addActionListener(this);
-        add(addButton);
+        add(addButton, c);
 
         setVisible(true);
 
@@ -43,6 +51,12 @@ public class AddProduct extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if(e.getSource()==addButton){
+            System.out.println("Item added successfully!");
+        }
+    }
+
+    public static void main(String[] args) {
+        new AddProduct();
     }
 }
