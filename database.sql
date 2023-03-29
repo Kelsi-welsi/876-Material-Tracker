7a2c6c5f15e8c738ff94cd0ae370640e2b196f80
Gashara <99560334+Gashara@users.noreply.github.com>
2023-03-29 12:32:12 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,83 @@
-DROP database IF EXISTS materials_db;
-CREATE DATABASE materials_db; -- create a new database
+-- Drop and recreate the materials database
+DROP DATABASE IF EXISTS materials_db;
+CREATE DATABASE materials_db;
+USE materials_db;
 
-USE materials_db; -- switch to the newly created database
+-- Create a table for login credentials
+CREATE TABLE login_credentials (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  user_type ENUM('Client', 'Admin', 'Quantity Surveyor', 'Supplier') NOT NULL,
+  PRIMARY KEY (user_id)
+);
 
+-- Create a table for suppliers
+CREATE TABLE suppliers (
+  supplier_id INT NOT NULL AUTO_INCREMENT,
+  supplier_name VARCHAR(50) NOT NULL,
+  supplier_contact_name VARCHAR(50) NOT NULL,
+  supplier_contact_email VARCHAR(50) NOT NULL,
+  supplier_contact_phone VARCHAR(20) NOT NULL,
+  PRIMARY KEY (supplier_id)
+);
+
+-- Create a table for shipping details
+CREATE TABLE shipping_details (
+  shipping_id INT NOT NULL AUTO_INCREMENT,
+  client_name VARCHAR(50) NOT NULL,
+  shipping_address VARCHAR(100) NOT NULL,
+  shipping_date DATE NOT NULL,
+  shipping_cost DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (shipping_id)
+);
+
+-- Create a table for projects
+CREATE TABLE projects (
+  project_id INT NOT NULL AUTO_INCREMENT,
+  project_name VARCHAR(50) NOT NULL,
+  project_description TEXT,
+  project_status ENUM('Approved', 'Pending', 'Not Approved') NOT NULL,
+  PRIMARY KEY (project_id)
+);
+
+-- Create the materials table
 CREATE TABLE materials (
-  material_number INT NOT NULL auto_increment,
+  material_number INT NOT NULL AUTO_INCREMENT,
   material_name VARCHAR(50) NOT NULL,
   description TEXT,
-  material_cost integer NOT NULL,
-  PRIMARY KEY(material_number)
-  
-); -- create a table for materials with columns for material number, material name, and description
+  material_cost INT NOT NULL,
+  PRIMARY KEY (material_number)
+);
+
+-- Insert sample data into the login_credentials table
+INSERT INTO login_credentials (username, password, user_type)
+VALUES
+  ('client1', 'password1', 'Client'),
+  ('client2', 'password2', 'Client'),
+  ('admin', 'adminpass', 'Admin'),
+  ('surveyor1', 'surveyorpass1', 'Quantity Surveyor'),
+  ('supplier1', 'supplierpass1', 'Supplier'),
+  ('supplier2', 'supplierpass2', 'Supplier');
+
+-- Insert sample data into the suppliers table
+INSERT INTO suppliers (supplier_name, supplier_contact_name, supplier_contact_email, supplier_contact_phone)
+VALUES
+  ('ABC Company', 'John Smith', 'john@abccompany.com', '555-1234'),
+  ('XYZ Inc.', 'Jane Doe', 'jane@xyzinc.com', '555-5678');
+
+-- Insert sample data into the shipping_details table
+INSERT INTO shipping_details (client_name, shipping_address, shipping_date, shipping_cost)
+VALUES
+  ('Client A', '123 Main St., Anytown USA', '2023-04-01', 25.00),
+  ('Client B', '456 High St., Anytown USA', '2023-04-03', 35.00);
+
+-- Insert sample data into the projects table
+INSERT INTO projects (project_name, project_description, project_status)
+VALUES
+  ('Project 1', 'Description of Project 1', 'Approved'),
+  ('Project 2', 'Description of Project 2', 'Pending'),
+  ('Project 3', 'Description of Project 3', 'Not Approved');
 
 INSERT INTO materials
 VALUES
